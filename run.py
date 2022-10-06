851f6578363cbb74db55912b4c3ee268e33620fb
Zdenek Kasner <zdenek.kasner@gmail.com>
2022-10-06 20:15:00 +0200

    Speed up application boot

--- run.py
+++ run.py
@@ -2,28 +2,12 @@
 
 import os
 import sys
-import argparse
-import socket
 
 sys.path.append(os.path.dirname(__name__))
 
-from app import app, main
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("-t", "--cache_update_time", type=int, default=60,
-    #     help="Time in minutes after cache with menus should be updated.")
-
-    return parser.parse_args()
-
-def main():
-    args = parse_args()
-    main(args)
-    return app
-
+from app import app, create_app
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args)
+    create_app()
     
     app.run(debug=True)
