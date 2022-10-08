76cd3a0d1d5d3dd7b742f2bf235a4b17ba3fccfa
Zdenek Kasner <zdenek.kasner@gmail.com>
2022-10-08 20:37:52 +0200

    Fix empty menus

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,14 @@
 import os
 import requests
 import json
-import socket
 import logging
 import datetime
 import random
 from datetime import timedelta
-from flask import Flask, render_template, jsonify, request, url_for
-from collections import defaultdict
+from flask import Flask, render_template, jsonify
 from slack_sdk import WebClient
 from flask_apscheduler import APScheduler
 
-from .places import Menu, Dish
 from .places import MenzaTroja, BufetTroja, CastleRestaurant
 
 scheduler = APScheduler()
@@ -86,32 +83,41 @@ def get_cache_age():
     return now - app.config['last_update']
 
 
-def generate_meal_of_the_day():
+def generate_dish_of_the_day():
     now = datetime.datetime.now()
 
     overview = get_overview_for_day(now.date())
-    place = random.choice(overview)
+    places_with_dishes = [p for p in overview if p["dishes"]]
+
+    if not places_with_dishes:
+        app.config["dish_of_the_day"] = {
+            "place" : "The Restaurant at the End of the Universe",
+            "dish" : "Ameglian Major Cow"
+        }
+        return
+
+    place = random.choice(places_with_dishes)
     place_name = place["name"]
 
     dish = random.choice(place["dishes"])
     dish_name = dish["name_en"] or dish["name"]
 
-    app.config["meal_of_the_day"] = {
+    app.config["dish_of_the_day"] = {
         "place" : place_name,
         "dish" : dish_name
     }
 
 
-@app.route('/motd', methods=['GET'])
-def meal_of_the_day():
-    if not get_meal_of_the_day():
-        generate_meal_of_the_day()
+@app.route('/dotd', methods=['GET'])
+def dish_of_the_day():
+    if not get_dish_of_the_day():
+        generate_dish_of_the_day()
 
-    return get_meal_of_the_day()["dish"], 200
+    return get_dish_of_the_day()["dish"], 200
 
 
-def get_meal_of_the_day():
-    return app.config.get("meal_of_the_day")
+def get_dish_of_the_day():
+    return app.config.get("dish_of_the_day")
     
 
 @app.route('/', methods=['GET', 'POST'])
@@ -120,19 +126,19 @@ def index():
     
     now = datetime.datetime.now()
     overview = get_overview_for_day(now.date())
-    generate_meal_of_the_day()
+    generate_dish_of_the_day()
     last_update = app.config['last_update'].strftime("%d %b %Y %H:%M:%S")
     
     return render_template('index.html', 
         date=now, 
         overview=overview,
         last_update=last_update,
-        motd=get_meal_of_the_day()
+        dotd=get_dish_of_the_day()
     )
 
-@app.route('/test_motd', methods=['GET', 'POST'])
-def test_motd():
-    generate_meal_of_the_day()
+@app.route('/test_dotd', methods=['GET', 'POST'])
+def test_dotd():
+    generate_dish_of_the_day()
     return success()
 
 
@@ -143,7 +149,7 @@ def test_invite():
 
 
 def send_lunch_invite():
-    place_name, dish_name = app.config["meal_of_the_day"]
+    place_name, dish_name = app.config["dish_of_the_day"]
     
     now = datetime.datetime.now()
     now_str = now.strftime("%Y-%m-%d")
@@ -158,7 +164,7 @@ def send_lunch_invite():
             "accessory": {
                 "type": "image",
                 "image_url": f"http://ufallab.ms.mff.cuni.cz/~kasner/cfm/{now_str}.png",
-                "alt_text": "meal of the day"
+                "alt_text": "dish of the day"
             }
         },
         {
@@ -198,7 +204,7 @@ def create_app(*args, **kwargs):
     app.config['last_update'] = None
 
     scheduler.add_job(id='fetch', func=reload_places, trigger="cron", hour=7, day_of_week="mon,tue,wed,thu,fri")
-    scheduler.add_job(id='motd', func=generate_meal_of_the_day, trigger="cron", hour=8, day_of_week="mon,tue,wed,thu,fri")
+    scheduler.add_job(id='dotd', func=generate_dish_of_the_day, trigger="cron", hour=8, day_of_week="mon,tue,wed,thu,fri")
     scheduler.add_job(id='invite', func=send_lunch_invite, trigger="cron", hour=12, day_of_week="mon,tue,wed,thu,fri")
     scheduler.start()
     
